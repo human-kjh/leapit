377da63c98e5135707be80b768b338c70f1982fb
harimmon <bhr0502@gmail.com>
2025-04-22 20:13:57 +0900

    [feat] 채용 공고 등록

--- src/main/java/com/example/leapit/jobposting/JobPosting.java
+++ src/main/java/com/example/leapit/jobposting/JobPosting.java
@@ -2,15 +2,19 @@ package com.example.leapit.jobposting;
 
 import com.example.leapit.user.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
+@Setter
 @Table(name = "job_posting_tb")
 @Entity
 public class JobPosting {
@@ -19,10 +23,15 @@ public class JobPosting {
     private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String positionType;
+
     private Integer minCareerLevel;
     private Integer maxCareerLevel;
     private String educationLevel;
@@ -33,12 +42,15 @@ public class JobPosting {
     @Lob
     private String serviceIntro;
 
+    @Column(nullable = false)
     private LocalDate deadline;
 
     @Lob
+    @Column(nullable = false)
     private String responsibility;
 
     @Lob
+    @Column(nullable = false)
     private String qualification;
 
     @Lob
@@ -49,8 +61,42 @@ public class JobPosting {
 
     @Lob
     private String additionalInfo;
-    private Integer viewCount;
+
+    @Column(columnDefinition = "integer default 0", nullable = false)
+    private Integer viewCount = 0;
 
     @CreationTimestamp
     private Timestamp createdAt;
+
+    @ElementCollection
+    @CollectionTable(name = "job_posting_tech_stack_tb", joinColumns = @JoinColumn(name = "job_posting_id"))
+    @Column(name = "techStack", nullable = false)
+    private List<String> techStack;
+
+    @Builder
+    public JobPosting(User user, String title, String positionType,
+                      Integer minCareerLevel, Integer maxCareerLevel,
+                      String educationLevel, Integer addressRegionId, Integer addressSubRegionId,
+                      String addressDetail, String serviceIntro, LocalDate deadline,
+                      String responsibility, String qualification, String preference,
+                      String benefit, String additionalInfo, List<String> techStack) {
+        this.user = user;
+        this.title = title;
+        this.positionType = positionType;
+        this.minCareerLevel = minCareerLevel;
+        this.maxCareerLevel = maxCareerLevel;
+        this.educationLevel = educationLevel;
+        this.addressRegionId = addressRegionId;
+        this.addressSubRegionId = addressSubRegionId;
+        this.addressDetail = addressDetail;
+        this.serviceIntro = serviceIntro;
+        this.deadline = deadline;
+        this.responsibility = responsibility;
+        this.qualification = qualification;
+        this.preference = preference;
+        this.benefit = benefit;
+        this.additionalInfo = additionalInfo;
+        this.techStack = techStack;
+        this.viewCount = 0;
+    }
 }
