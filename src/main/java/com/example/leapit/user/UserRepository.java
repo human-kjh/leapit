a40207ca5af1bafff18631f94bd0f81e49fc3e0a
hahamik <dlapdl0615@gmail.com>
2025-04-22 15:36:26 +0900

    회원가입

--- src/main/java/com/example/leapit/user/UserRepository.java
+++ src/main/java/com/example/leapit/user/UserRepository.java
@@ -8,4 +8,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
     private final EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findByUsername(String username) {
+        try {
+            return em.createQuery("select u from User u where u.username = :username", User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
 }
