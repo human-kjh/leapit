369a8230fc156ee2cf165027837aa348f3ab02c0
jay <wngml0708@gmail.com>
2025-04-22 15:38:43 +0900

    이력서 목록 조회 (userId 하드 코딩)

--- src/main/java/com/example/leapit/resume/ResumeController.java
+++ src/main/java/com/example/leapit/resume/ResumeController.java
@@ -1,10 +1,28 @@
 package com.example.leapit.resume;
 
+import com.example.leapit.user.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class ResumeController {
     private final ResumeService resumeService;
+    private final HttpSession session;
+
+    @GetMapping("/resume/list")
+    public String resumeList(HttpServletRequest request) {
+        // session 구현 전
+        // 로그인 한 유저의 이력서들만 출력된다.
+        Integer userId = 2;
+
+        List<Resume> resumeList = resumeService.list(userId);
+        request.setAttribute("models", resumeList);
+        return "personal/resume/list";
+    }
 }
--- src/main/java/com/example/leapit/resume/ResumeService.java
+++ src/main/java/com/example/leapit/resume/ResumeService.java
@@ -3,8 +3,15 @@ package com.example.leapit.resume;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ResumeService {
     private final ResumeRepository resumeRepository;
+
+    public List<Resume> list(int userId) {
+        // 자신의 userId로 된 모든 resume을 찾아서 return
+        return resumeRepository.findAllById(userId);
+    }
 }
