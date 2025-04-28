ebe5c9218c6318ed6633ff0b050ec4d51560ad12
parangdajavous <nuit0535@gmail.com>
2025-04-28 09:09:24 +0900

    [feat] 커뮤니티 게시글 목록 (#38)
    BoardResponse.ListDTO 생성
    
    게시글 목록 C, S, R
    
    personal-header 커뮤니티 주소 연결
    
    게시글 작성한 user만 드롭다운 버튼 뜨도록 구현
    
    게시글 클릭(상세보기), 수정, 삭제 버튼 클릭 시 페이지 이동 주소연결 완료

--- src/main/java/com/example/leapit/board/BoardController.java
+++ src/main/java/com/example/leapit/board/BoardController.java
@@ -1,10 +1,33 @@
 package com.example.leapit.board;
 
+import com.example.leapit.user.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
     private final BoardService boardService;
+    private final HttpSession session;
+
+
+    @GetMapping("/community/list")
+    public String list(HttpServletRequest request) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser == null) {
+            List<BoardResponse.ListDTO> respDTO = boardService.list(null);
+            request.setAttribute("models", respDTO);
+        } else {
+            List<BoardResponse.ListDTO> respDTO = boardService.list(sessionUser.getId());
+            request.setAttribute("models", respDTO);
+        }
+
+        return "personal/board/list";
+    }
 }
